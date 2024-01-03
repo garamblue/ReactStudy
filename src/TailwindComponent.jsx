dd35b7899847766cb96c5a999de97495b1857744
garamblue <hemiblues@gmail.com>
2024-01-03 10:13:33 +0900

    1st commit

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className="text-7xl">안녕!</h1>
+      <button className="bg-blue-900 rounded-xl px-2 text-white">Button</button>
+    </div>
+  );
+}
